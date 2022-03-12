4db3102c11394a93f2eb0ac2080fa546cdadecfe
KevinGodek <92218951+KevinGodek@users.noreply.github.com>
2022-03-12 18:15:48 -0500

    added query to schema file

--- dev/employeeTrackerSchema.sql
+++ dev/employeeTrackerSchema.sql
@@ -5,8 +5,8 @@ USE employeetracker_DB;
 
 CREATE TABLE employee (
     employeeID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL, 
-    last_name VARCHAR(30) NOT NULL,
+    firstName VARCHAR(30) NOT NULL, 
+    lastName VARCHAR(30) NOT NULL,
     roleID INT NOT NULL,
     managerID INT,
     FOREIGN KEY (roleID) REFERENCES role(roleID),
@@ -28,3 +28,12 @@ CREATE TABLE department (
     departmentID INT,
     FOREIGN KEY (departmentID) REFERENCES department (departmentID)
 );
+
+SELECT employee.firstName, 
+	employee.lastName,
+	role.title AS Title,
+    role.salary AS Salary,
+    department.name AS Department
+FROM employee 
+	INNER JOIN role ON employee.roleID=role.roleID
+    INNER JOIN department ON employee.roleID=department.departmentID
--- dev/employeeTrackerSeeds.sql
+++ dev/employeeTrackerSeeds.sql
@@ -1,14 +1,14 @@
-INSERT INTO employee (first_name, last_name) VALUES ("Stan","Marsh");
-INSERT INTO employee (first_name, last_name) VALUES ("Kyle","Broflovski");
-INSERT INTO employee (first_name, last_name) VALUES ("Eric","Cartman");
-INSERT INTO employee (first_name, last_name) VALUES ("Kenny","McCormick");
-INSERT INTO employee (first_name, last_name) VALUES ("Randy","Marsh");
-INSERT INTO employee (first_name, last_name) VALUES ("Will","Smith");
-INSERT INTO employee (first_name, last_name) VALUES ("Micheal","Kelso");
-INSERT INTO employee (first_name, last_name) VALUES ("Red","Foreman");
-INSERT INTO employee (first_name, last_name) VALUES ("BoJack","Horseman");
-INSERT INTO employee (first_name, last_name) VALUES ("Rick","Sanchez");
-INSERT INTO employee (first_name, last_name) VALUES ("Stephan","Curry");
+INSERT INTO employee (firstName, lastName) VALUES ("Stan","Marsh");
+INSERT INTO employee (firstName, lastName) VALUES ("Kyle","Broflovski");
+INSERT INTO employee (firstName, lastName) VALUES ("Eric","Cartman");
+INSERT INTO employee (firstName, lastName) VALUES ("Kenny","McCormick");
+INSERT INTO employee (firstName, lastName) VALUES ("Randy","Marsh");
+INSERT INTO employee (firstName, lastName) VALUES ("Will","Smith");
+INSERT INTO employee (firstName, lastName) VALUES ("Micheal","Kelso");
+INSERT INTO employee (firstName, lastName) VALUES ("Red","Foreman");
+INSERT INTO employee (firstName, lastName) VALUES ("BoJack","Horseman");
+INSERT INTO employee (firstName, lastName) VALUES ("Rick","Sanchez");
+INSERT INTO employee (firstName, lastName) VALUES ("Stephan","Curry");
 
 INSERT INTO role(title, salary) VALUES ('Senior Engineer', 110000);
 INSERT INTO role(title, salary) VALUES ('Junior Engineer', 80000);
