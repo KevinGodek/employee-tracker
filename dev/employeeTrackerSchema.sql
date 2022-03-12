dc66103c118089927646db6fd20e40c3908e9a28
KevinGodek <92218951+KevinGodek@users.noreply.github.com>
2022-03-12 18:02:40 -0500

    updated schema file with foreign keys

--- dev/employeeTrackerSchema.sql
+++ dev/employeeTrackerSchema.sql
@@ -4,21 +4,27 @@ CREATE DATABASE employeetracker_DB;
 USE employeetracker_DB;
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(role_id),
+    FOREIGN KEY (manager_id) REFERENCES department(department_id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL,
     department_id INT
+    FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department (department_id)
+);
---
+++ dev/employeeTrackerSeeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO employee (first_name, last_name) VALUES ("Stan","Marsh");
+INSERT INTO employee (first_name, last_name) VALUES ("Kyle","Broflovski");
+INSERT INTO employee (first_name, last_name) VALUES ("Eric","Cartman");
+INSERT INTO employee (first_name, last_name) VALUES ("Kenny","McCormick");
+INSERT INTO employee (first_name, last_name) VALUES ("Randy","Marsh");
+INSERT INTO employee (first_name, last_name) VALUES ("Will","Smith");
+INSERT INTO employee (first_name, last_name) VALUES ("Micheal","Kelso");
+INSERT INTO employee (first_name, last_name) VALUES ("Red","Foreman");
+INSERT INTO employee (first_name, last_name) VALUES ("BoJack","Horseman");
+INSERT INTO employee (first_name, last_name) VALUES ("Rick","Sanchez");
+INSERT INTO employee (first_name, last_name) VALUES ("Stephan","Curry");
